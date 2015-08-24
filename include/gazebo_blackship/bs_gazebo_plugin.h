755440c22e758b90d9dde99062428953b4e806c0
mu-777 <ryosuke.murata.7@gmail.com>
2015-08-24 15:03:08 +0900

    make a plugin for bs

---
+++ include/gazebo_blackship/bs_gazebo_plugin.h
@@ -0,0 +1,122 @@
+#ifndef BS_GAZEBO_PLUGINS_H
+#define  BS_GAZEBO_PLUGINS_H
+
+#include "gazebo/physics/physics.hh"
+#include "gazebo/physics/PhysicsTypes.hh"
+#include "gazebo/sensors/SensorTypes.hh"
+#include "gazebo/transport/TransportTypes.hh"
+#include "gazebo/common/Time.hh"
+#include "gazebo/common/Plugin.hh"
+#include "gazebo/common/Events.hh"
+
+#include <geometry_msgs/TwistStamped.h>
+#include <ros/ros.h>
+
+// Custom Callback Queue
+#include <ros/callback_queue.h>
+#include <ros/advertise_options.h>
+
+
+namespace gazebo
+{
+    struct WheelAngularVelocity{
+        float front_left;
+        float front_right;
+        float rear_left;
+        float rear_right;
+    };
+
+    class BlackshipWheel{
+    public:
+        BlackshipWheel():is_initialized(false){};
+        ~BlackshipWheel(){};
+        void initialize(physics::ModelPtr _model, std::string _joint_name){
+            model_ = _model;
+            axis_joint_ = model_->GetJoint(_joint_name);
+            is_initialized = true;
+        };
+        void update(double _ang_vel, double _torque ){
+            if (is_initialized){
+                axis_joint_->SetVelocity( 0, _ang_vel);
+                axis_joint_->SetMaxForce( 0, _torque );
+            } else {
+                std::cout<<"Not initialized!" << std::endl;
+            }
+        };
+    private:
+        bool is_initialized;
+        physics::JointPtr axis_joint_;
+        physics::ModelPtr model_;
+    };
+
+
+
+  class BlackshipPlugin : public ModelPlugin
+  {
+    public:
+      BlackshipPlugin();
+      virtual ~BlackshipPlugin();
+      virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf );
+
+    protected:
+      virtual void UpdateChild();
+      virtual void FiniChild();
+
+    private:
+      void OnContact(const std::string &name, const physics::Contact &contact);
+      void OnDrive( const geometry_msgs::TwistStampedConstPtr &msg);
+      template <class T>
+      T getSDFElement(sdf::ElementPtr _sdf, const std::string& element_name, const T& default_element) {
+          if (_sdf->HasElement(element_name)){
+              return _sdf->GetElement(element_name)->Get<T>();
+          } else{
+              return default_element;
+          }
+      };
+
+      BlackshipWheel wheel_fl_;
+      BlackshipWheel wheel_fr_;
+      BlackshipWheel wheel_rl_;
+      BlackshipWheel wheel_rr_;
+
+      /// Parameters
+      std::string node_namespace_;
+      float wheel_radius_;
+      float wheel_track_;
+      float wheel_torque_;
+
+      ros::NodeHandle *rosnode_;
+      ros::Subscriber drive_sub_;
+
+      physics::WorldPtr world_;
+      physics::ModelPtr model_;
+      sensors::SensorPtr parent_sensor_;
+
+      /// Speeds of the wheels
+      WheelAngularVelocity wheel_ang_vel_;
+
+      // Simulation time of the last update
+      common::Time prev_update_time_;
+      common::Time last_cmd_vel_time_;
+
+      void spin();
+      boost::thread *spinner_thread_;
+      event::ConnectionPtr contact_event_;
+
+      // Pointer to the update event connection
+      event::ConnectionPtr updateConnection;
+  };
+}
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/bs_gazebo_plugin.cpp
@@ -0,0 +1,106 @@
+
+#include <boost/thread.hpp>
+#include <gazebo_blackship/bs_gazebo_plugin.h>
+#include <ros/time.h>
+
+using namespace gazebo;
+
+
+BlackshipPlugin::BlackshipPlugin():
+    wheel_fl_(), wheel_fr_(), wheel_rl_(), wheel_rr_()
+{
+    // Initialize the ROS node and subscribe to cmd_vel
+    int argc = 0;
+    char** argv = NULL;
+    ros::init(argc, argv, "gazebo_blackship", ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
+    rosnode_ = new ros::NodeHandle( node_namespace_ );
+
+    drive_sub_ = rosnode_->subscribe("cmd_vel", 1, &BlackshipPlugin::OnDrive, this );
+}
+
+BlackshipPlugin::~BlackshipPlugin()
+{
+  delete rosnode_;
+  delete spinner_thread_;
+}
+
+void BlackshipPlugin::FiniChild()
+{
+  rosnode_->shutdown();
+  spinner_thread_->join();
+}
+
+void BlackshipPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
+{
+  this->model_ = _parent;
+  this->world_ = this->model_->GetWorld();
+
+  this->node_namespace_ = getSDFElement(_sdf, "robotNamespace", std::string("")) + "/";
+  std::string fl_joint_name = getSDFElement(_sdf, "frontLeftWheelJoint", std::string("joint_blackship_wheel_frontleft"));
+  std::string fr_joint_name = getSDFElement(_sdf, "frontRightWheelJoint", std::string("joint_blackship_wheel_frontright"));
+  std::string rl_joint_name = getSDFElement(_sdf, "rearLeftWheelJoint", std::string("joint_blackship_wheel_rearleft"));
+  std::string rr_joint_name = getSDFElement(_sdf, "rearRightWheelJoint",std::string("joint_blackship_wheel_rearright"));
+  wheel_torque_ = getSDFElement(_sdf, "wheelTorque", 15.0);
+  wheel_radius_ = getSDFElement(_sdf, "wheelRadius", 0.160);
+  wheel_track_ = getSDFElement(_sdf, "wheelRadius", 0.340);
+
+  // Get the name of the parent model
+  std::string modelName = _sdf->GetParent()->Get<std::string>("name");
+  gzdbg << "plugin model name: " << modelName << "\n";
+
+  this->wheel_fl_.initialize(this->model_, fl_joint_name);
+  this->wheel_fr_.initialize(this->model_, fr_joint_name);
+  this->wheel_rl_.initialize(this->model_, rl_joint_name);
+  this->wheel_rr_.initialize(this->model_, rr_joint_name);
+
+  prev_update_time_ = 0;
+  last_cmd_vel_time_ = 0;
+
+  //initialize time and odometry position
+  prev_update_time_ = last_cmd_vel_time_ = this->world_->GetSimTime();
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->spinner_thread_ = new boost::thread( boost::bind( &BlackshipPlugin::spin, this) );
+  this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&BlackshipPlugin::UpdateChild, this));
+}
+
+
+void BlackshipPlugin::UpdateChild()
+{
+  common::Time time_now = this->world_->GetSimTime();
+  common::Time step_time = time_now - prev_update_time_;
+  prev_update_time_ = time_now;
+
+  wheel_fl_.update(wheel_ang_vel_.front_left, wheel_torque_);
+  wheel_fr_.update(wheel_ang_vel_.front_right, wheel_torque_);
+  wheel_rl_.update(wheel_ang_vel_.rear_left, wheel_torque_);
+  wheel_rr_.update(wheel_ang_vel_.rear_right, wheel_torque_);
+
+  // Timeout if we haven't received a cmd in <0.1 s
+  common::Time time_since_last_cmd = time_now - last_cmd_vel_time_;
+  if (time_since_last_cmd.Double() > 0.1)
+  {
+    wheel_ang_vel_.front_left = 0;
+    wheel_ang_vel_.front_right = 0;
+    wheel_ang_vel_.rear_left = 0;
+    wheel_ang_vel_.rear_right = 0;
+  }
+}
+
+
+void BlackshipPlugin::OnDrive( const geometry_msgs::TwistStampedConstPtr &msg)
+{
+  last_cmd_vel_time_ = this->world_->GetSimTime();
+  float v = msg->twist.linear.x;
+  float w = msg->twist.angular.z;
+  wheel_ang_vel_.front_right = wheel_ang_vel_.rear_right = (1/wheel_radius_)*(v + w*wheel_track_/2.0);
+  wheel_ang_vel_.front_left = wheel_ang_vel_.rear_left = (1/wheel_radius_)*(v - w*wheel_track_/2.0);
+}
+
+void BlackshipPlugin::spin()
+{
+  while(ros::ok()) ros::spinOnce();
+}
+
+GZ_REGISTER_MODEL_PLUGIN(BlackshipPlugin);
